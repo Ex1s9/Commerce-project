0299284b8fde278587068bf1b3b4c0844ba02c3c
kryptoNkn <myersalp@gmail.com>
2025-02-09 12:22:57 +0300

    Changing the SCSS reference

--- src/layouts/topbar/topbar.tsx
+++ src/layouts/topbar/topbar.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import styles from './topbar.module.scss';
+import s from './topbar.module.scss';
 
 const Topbar: React.FC = () => {
     const photo = "/images/FooterLogo.jpeg";
     return (
-        <header className={styles.header}>
-            <div className={styles.headerContent}>
-                <div className={styles.logo}>
-                    <div className={styles.item}>
+        <header className={s.header}>
+            <div className={s.headerContent}>
+                <div className={s.logo}>
+                    <div className={s.item}>
                         <a href="#"><img src={photo}/></a>
                     </div>
                 </div>
-                <div className={styles.addressTextBox}>
-                    <p className={styles.addressText}>
+                <div className={s.addressTextBox}>
+                    <p className={s.addressText}>
                         <a href="#">Севастополь, улица Токарева, 18В</a>
                     </p>
                 </div>
                 <a href="#">
-                    <button className={styles.onlineButton}>Онлайн запись</button>
+                    <button className={s.onlineButton}>Онлайн запись</button>
                 </a>
-                <div className={styles.topbarline}></div>
+                <div className={s.topbarline}></div>
             </div>
         </header>
     );
