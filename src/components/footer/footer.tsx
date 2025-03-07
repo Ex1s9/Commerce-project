d8114b581315514be9355ca3f8ebdbd47cb56aff
kryptoNkn <myersalp@gmail.com>
2025-03-07 13:35:09 +0300

    added icons and links

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,6 +1,9 @@
 import s from './footer.module.scss';
 import React from "react";
 
+import { RiTelegram2Fill } from "react-icons/ri";
+import { FaWhatsapp } from "react-icons/fa";
+
 const Footer: React.FC = () => {
 
     const listBar = [
@@ -15,13 +18,22 @@ const Footer: React.FC = () => {
         }
     ];
 
+    const links = [
+        "https://t.me/+79783460526",
+        "https://api.whatsapp.com/send/?phone=79783460526&text&type=phone_number&app_absent=0"
+    ];
+
     return (
         <footer>
             <div className={s.container}>
-                <p className={s.nameTxt}>In Balance</p>
+                <div className={s.firstLine}>
+                    <p className={s.nameTxt}>In Balance</p>
+                    <a href={links[0]} className={s.tglIcon}><RiTelegram2Fill/></a>
+                    <a href={links[1]} className={s.tglIcon}><FaWhatsapp/></a>
+                </div>
                 <div className={s.mainFooterLine}>
                     <div className={s.contacts}>
-                        <p className={s.phone}>+7 (978) 346-05-26</p>
+                    <p className={s.phone}>+7 (978) 346-05-26</p>
                         <p className={s.date}>Пн-Вс: 10:00 - 22:00</p>
                     </div>
                     <div className={s.footerMenu}>
