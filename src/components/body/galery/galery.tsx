1bdb45ea4effb60342666e5d6eef63ec7003cb66
Ex1s9 <sadovskyroma@gmail.com>
2025-03-09 11:10:45 +0300

    galery

--- src/components/body/galery/galery.tsx
+++ src/components/body/galery/galery.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "../../../../node_modules/swiper/modules/navigation.scss";
 import "../../../../node_modules/swiper/modules/pagination.scss";
@@ -6,17 +7,26 @@ import "../../../../node_modules/swiper/swiper.scss";
 import s from "./galery.module.scss";
 
 const images = [
-     "/galery/IMG_1606.JPG",
-     "/galery/IMG_2710.PNG",
-     "/galery/IMG_2711.PNG",
-     "/galery/IMG_2714.PNG",
-     "/galery/IMG_2998.DNG",
-     "/galery/IMG_3551.JPG",
-     "/galery/IMG_3582.JPG"
+     "/galery/IMG_0126.webp",
+     "/galery/IMG_0527.webp",
+     "/galery/IMG_1606.webp",
+     "/galery/IMG_2033.webp",
+     "/galery/IMG_2035.webp",
+     "/galery/IMG_2036.webp",
+     "/galery/IMG_2037.webp",
+     "/galery/IMG_2710.webp",
+     "/galery/IMG_2711.webp",
+     "/galery/IMG_2714.webp",
+     "/galery/IMG_2998.webp",
+     "/galery/IMG_3459.webp",
+     "/galery/IMG_3551.webp",
+     "/galery/IMG_3582.webp"
 ];
 
 const Galery = () => {
      const [activeIndex, setActiveIndex] = useState(0);
+     const prevRef = useRef<HTMLButtonElement>(null);
+     const nextRef = useRef<HTMLButtonElement>(null);
 
      return (
           <main id="galery" className={s.galery}>
@@ -25,31 +35,53 @@ const Galery = () => {
                     <a>Смотреть все работы</a>
                </div>
 
-               <Swiper
-                    initialSlide={1}
-                    centeredSlides={true}
-                    onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
-                    slidesPerView={1}
-                    spaceBetween={90}
-                    autoplay={false}
-                    loop
-                    allowTouchMove={true}
-                    className={s.swiper}
-                    breakpoints={{
-                         990: {
-                              slidesPerView: "auto",
-                         }
-                    }}
-               >
-                    {images.map((imgSrc, i) => (
-                         <SwiperSlide
-                              key={imgSrc}
-                              className={`${s.slide} ${i === activeIndex ? s.activeSlide : ""}`}
-                         >
-                              <img src={imgSrc} alt="galery" />
-                         </SwiperSlide>
-                    ))}
-               </Swiper>
+               <div className={s.swiperWrapper}>
+                    {/* Кастомные кнопки навигации */}
+                    <button ref={prevRef} className={s.customPrev}></button>
+                    <button ref={nextRef} className={s.customNext}></button>
+
+                    <Swiper
+                         modules={[Navigation, Pagination]}
+                         initialSlide={1}
+                         centeredSlides={true}
+                         onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
+                         slidesPerView={1}
+                         spaceBetween={90}
+                         loop
+                         allowTouchMove={true}
+                         navigation={{
+                              prevEl: prevRef.current,
+                              nextEl: nextRef.current,
+                         }}
+                         className={s.swiper}
+                         breakpoints={{
+                              990: {
+                                   slidesPerView: "auto",
+                              },
+                         }}
+                         onSwiper={(swiper) => {
+                              setTimeout(() => {
+                                   if (prevRef.current && nextRef.current) {
+                                        if (swiper.params.navigation && typeof swiper.params.navigation !== 'boolean') {
+                                             swiper.params.navigation.prevEl = prevRef.current;
+                                             swiper.params.navigation.nextEl = nextRef.current;
+                                             swiper.navigation.init();
+                                             swiper.navigation.update();
+                                        }
+                                   }
+                              });
+                         }}
+                    >
+                         {images.map((imgSrc, i) => (
+                              <SwiperSlide
+                                   key={imgSrc}
+                                   className={`${s.slide} ${i === activeIndex ? s.activeSlide : ""}`}
+                              >
+                                   <img src={imgSrc} alt="galery" />
+                              </SwiperSlide>
+                         ))}
+                    </Swiper>
+               </div>
           </main>
      );
 };
